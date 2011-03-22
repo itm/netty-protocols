4b54475df610d44f2e6b1664b25cb65375e72bca
pfisterer <github@farberg.de>
2011-03-22 18:43:05 +0100

    License updated

--- protocols/isense/src/main/java/de/uniluebeck/itm/nettyrxtx/isense/ISensePacketDecoderFactory.java
+++ protocols/isense/src/main/java/de/uniluebeck/itm/nettyrxtx/isense/ISensePacketDecoderFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010, Dennis Pfisterer, Institute of Telematics, University of Luebeck
+ * Copyright (c) 2010, Daniel Bimschas and Dennis Pfisterer, Institute of Telematics, University of Luebeck
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
