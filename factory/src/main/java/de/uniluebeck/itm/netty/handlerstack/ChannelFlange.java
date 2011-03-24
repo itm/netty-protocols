104474f83e8a798a48c369030980273beb080ba8
pfisterer <github@farberg.de>
2011-03-24 16:38:20 +0100

    Documentation and license header update

--- factory/src/main/java/de/uniluebeck/itm/netty/handlerstack/ChannelFlange.java
+++ factory/src/main/java/de/uniluebeck/itm/netty/handlerstack/ChannelFlange.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2010, Daniel Bimschas and Dennis Pfisterer, Institute of Telematics, University of Luebeck
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 	- Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * 	  disclaimer.
+ * 	- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * 	  following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the University of Luebeck nor the names of its contributors may be used to endorse or promote
+ * 	  products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.uniluebeck.itm.netty.handlerstack;
 
 import java.util.Random;
@@ -12,7 +34,23 @@ import org.jboss.netty.channel.SimpleChannelHandler;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * Used to interconnect two Netty channels via proxy handlers. Each proxy handler tries to stay on top of the pipeline
+ * of its corresponding channel. Whenever a message is received on one channel, it is written to the other channel.
+ * 
+ * To configure a channel flange, you must configure the left and the right side of the flange. To do so, you have two
+ * options:
+ * 
+ * i) Set the (left|right) channel using {@see #setLeftChannel(Channel)} or {@see #setRightChannel(Channel)}
+ * 
+ * ii) Get the (left|right) handler using {@see #getLeftHandler()} or {@see #getRigthHandler()} and add it to the top of
+ * a pipeline
+ * 
+ * @author Dennis Pfisterer
+ * 
+ */
 public class ChannelFlange {
+    /** Used to generate the handler's name */
     private static final Random r = new Random();
 
     class ProxyHandler extends SimpleChannelHandler {
@@ -26,9 +64,9 @@ public class ChannelFlange {
             ChannelPipeline pipeline = channel.getPipeline();
 
             if (!(pipeline.getLast() == this)) {
-                if( pipeline.toMap().entrySet().contains(this))
+                if (pipeline.toMap().entrySet().contains(this))
                     pipeline.remove(this);
-                
+
                 pipeline.addLast("proxy-" + id, this);
             }
         }
@@ -53,7 +91,7 @@ public class ChannelFlange {
 
             if (getOtherProxyHandler().getChannel() != null)
                 getOtherProxyHandler().getChannel().write(e.getMessage());
-            
+
             super.messageReceived(ctx, e);
         }
 
@@ -82,11 +120,11 @@ public class ChannelFlange {
     public void setLeftChannel(Channel leftChannel) {
         left.registerWith(leftChannel);
     }
-    
+
     public void setRightChannel(Channel rightChannel) {
         right.registerWith(rightChannel);
     }
-    
+
     public ChannelHandler getLeftHandler() {
         return left;
     }
