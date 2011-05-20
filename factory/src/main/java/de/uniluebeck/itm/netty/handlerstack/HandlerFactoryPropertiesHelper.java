c32aefabda7c6250850f94bca403ab113446c3ff
Dennis Pfisterer <github@farberg.de>
2011-05-20 16:47:53 +0200

    New helper class to extract properties from multimaps

---
+++ factory/src/main/java/de/uniluebeck/itm/netty/handlerstack/HandlerFactoryPropertiesHelper.java
@@ -0,0 +1,32 @@
+package de.uniluebeck.itm.netty.handlerstack;
+
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.Multimap;
+
+public class HandlerFactoryPropertiesHelper {
+
+    public static String getFirstValueOf(Multimap<String, String> properties, String name, String defaultValue) {
+        if (properties.containsKey(name))
+            return properties.get(name).iterator().next();
+        else
+            return defaultValue;
+    }
+
+    public static long getFirstValueOf(Multimap<String, String> properties, String name, long defaultValue) {
+        return Long.parseLong(getFirstValueOf(properties, name, "" + defaultValue));
+    }
+    
+    public static int getFirstValueOf(Multimap<String, String> properties, String name, int defaultValue) {
+        return Integer.parseInt(getFirstValueOf(properties, name, "" + defaultValue));
+    }
+    
+    public static short getFirstValueOf(Multimap<String, String> properties, String name, short defaultValue) {
+        return Short.parseShort(getFirstValueOf(properties, name, "" + defaultValue));
+    }
+    
+    public static TimeUnit getFirstValueOf(Multimap<String, String> properties, String name, TimeUnit defaultValue) {
+        return TimeUnit.valueOf(getFirstValueOf(properties, name, defaultValue.toString()));
+    }
+
+}
