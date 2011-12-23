1bdaae95f3910fc45757aa34cc10b278d35c5edd
Daniel Bimschas <daniel@bimschas.com>
2011-12-23 10:14:13 +0100

    Removed FilterPipelineEmbedder and related, now obsolete classes

--- filterpipeline/src/main/java/de/uniluebeck/itm/netty/handlerstack/FilterPipelineDownstreamListener.java
+++
@@ -1,9 +0,0 @@
-package de.uniluebeck.itm.netty.handlerstack;
-
-import org.jboss.netty.channel.ChannelDownstreamHandler;
-
-public interface FilterPipelineDownstreamListener extends ChannelDownstreamHandler {
-
-	void downstreamExceptionCaught(Throwable e);
-
-}
--- filterpipeline/src/main/java/de/uniluebeck/itm/netty/handlerstack/FilterPipelineUpstreamListener.java
+++
@@ -1,9 +0,0 @@
-package de.uniluebeck.itm.netty.handlerstack;
-
-import org.jboss.netty.channel.ChannelUpstreamHandler;
-
-public interface FilterPipelineUpstreamListener extends ChannelUpstreamHandler {
-
-	void upstreamExceptionCaught(Throwable e);
-
-}
