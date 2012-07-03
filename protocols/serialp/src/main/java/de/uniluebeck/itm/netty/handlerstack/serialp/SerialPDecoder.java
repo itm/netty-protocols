7999aa1879405a9e8ed72c98361ce3480865e975
Daniel Bimschas <daniel@bimschas.com>
2012-07-03 18:22:38 +0200

    Began implementing SerialP protocol decoder/encoder for #46 (Implement (de-)framing algorithm for TinyOS)

--- protocols/serialp/src/main/java/de/uniluebeck/itm/netty/handlerstack/serialp/SerialPDecoder.java
+++ protocols/serialp/src/main/java/de/uniluebeck/itm/netty/handlerstack/serialp/SerialPDecoder.java
@@ -1,5 +1,56 @@
 package de.uniluebeck.itm.netty.handlerstack.serialp;
 
-public class SerialPDecoder {
+import net.tinyos.util.Crc;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.oneone.OneToOneDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class SerialPDecoder extends OneToOneDecoder {
+
+	private static final Logger log = LoggerFactory.getLogger(SerialPDecoder.class);
+
+	@Override
+	protected Object decode(final ChannelHandlerContext ctx, final Channel channel, final Object msg) throws Exception {
+
+		final ChannelBuffer encoded = (ChannelBuffer) msg;
+		final int encodedBufferLength = encoded.readableBytes();
+
+		if (encodedBufferLength < 4) {
+			throw new IllegalArgumentException("Packet must be at least 4 bytes long!");
+		}
+
+		final byte packetType = encoded.getByte(0);
+		final byte firstByte = encoded.getByte(1);
+
+		final ChannelBuffer decoded = ChannelBuffers.buffer(encodedBufferLength - 4);
+
+		int crcCalculated = 0;
+
+		int crcRead = (encoded.getByte(encoded.readerIndex() + encoded.readableBytes() - 2) & 0xff)
+				| (encoded.getByte(encoded.readerIndex() + encoded.readableBytes() - 1) & 0xff) << 8;
+
+		crcCalculated = Crc.calcByte(crcCalculated, packetType);
+		crcCalculated = Crc.calcByte(crcCalculated, firstByte);
+
+		int payloadBegin = encoded.readerIndex() + 2;
+		int payloadEnd = encoded.writerIndex() - 2;
+
+		byte currentByte;
+		for (int i = payloadBegin; i < payloadEnd; i++) {
+			currentByte = encoded.getByte(i);
+			decoded.writeByte(currentByte);
+			crcCalculated = Crc.calcByte(crcCalculated, currentByte);
+		}
+
+		if (crcCalculated != crcRead) {
+			log.trace("CRC mismatch, discarding packet {}", encoded);
+			return null;
+		}
+
+		return decoded;
+	}
 }
--- protocols/serialp/src/main/java/de/uniluebeck/itm/netty/handlerstack/serialp/SerialPEncoder.java
+++ protocols/serialp/src/main/java/de/uniluebeck/itm/netty/handlerstack/serialp/SerialPEncoder.java
@@ -1,5 +1,46 @@
 package de.uniluebeck.itm.netty.handlerstack.serialp;
 
-public class SerialPEncoder {
+import net.tinyos.util.Crc;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
 
+public class SerialPEncoder extends OneToOneEncoder {
+
+	private static final int PACKET_TYPE = 0x45 & 0xFF;
+
+	private static final int FIRST_BYTE = 0x00;
+
+	@Override
+	protected Object encode(final ChannelHandlerContext ctx, final Channel channel, final Object msg) throws Exception {
+
+		final ChannelBuffer decoded = (ChannelBuffer) msg;
+		final int decodedLength = decoded.readableBytes();
+		final ChannelBuffer encoded = ChannelBuffers.buffer(decodedLength + 4);
+
+		int crc = 0;
+
+		encoded.writeByte(PACKET_TYPE);
+		encoded.writeByte(FIRST_BYTE);
+
+		crc = Crc.calcByte(crc, PACKET_TYPE);
+		crc = Crc.calcByte(crc, FIRST_BYTE);
+
+		byte currentByte;
+		final int decodedReaderIndex = decoded.readerIndex();
+
+		for (int i = 0; i < decodedLength; i++) {
+
+			currentByte = decoded.getByte(decodedReaderIndex + i);
+			encoded.writeByte(currentByte);
+			crc = Crc.calcByte(crc, currentByte);
+		}
+
+		encoded.writeByte(crc & 0xFF);
+		encoded.writeByte(crc >> 8);
+
+		return encoded;
+	}
 }
