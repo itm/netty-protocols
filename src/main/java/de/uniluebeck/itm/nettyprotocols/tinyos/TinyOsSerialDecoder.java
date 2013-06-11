4e82cc715d1fb279042996b3c9e1dc4a537b3603
Daniel Bimschas <daniel@bimschas.com>
2013-06-11 10:29:55 +0200

    Fixed #65 (Faulty decoding of TinyOS packets)

--- src/main/java/de/uniluebeck/itm/nettyprotocols/tinyos/TinyOsSerialDecoder.java
+++ src/main/java/de/uniluebeck/itm/nettyprotocols/tinyos/TinyOsSerialDecoder.java
@@ -10,6 +10,12 @@ import org.slf4j.LoggerFactory;
 
 public class TinyOsSerialDecoder extends OneToOneDecoder {
 
+	private static final int PROTO_PACKET_NOACK = 69;
+
+	private static final int PROTO_PACKET_ACK = 68;
+
+	private static final int PROTO_ACK = 67;
+
 	private final Logger log;
 
 	public TinyOsSerialDecoder() {
@@ -31,22 +37,33 @@ public class TinyOsSerialDecoder extends OneToOneDecoder {
 		}
 
 		final byte packetType = encoded.getByte(0);
-		final byte firstByte = encoded.getByte(1);
+		final byte sequenceNumber = encoded.getByte(1);
+
+		int payloadBegin = encoded.readerIndex() + 2;
+		int payloadEnd = encoded.writerIndex() - 2;
+
+		switch (packetType) {
+			case PROTO_PACKET_NOACK:
+				// nothing to do as packet does not need to be acknowledged
+				break;
+			case PROTO_PACKET_ACK:
+				// TODO implement acknowledgment of packet
+				break;
+			case PROTO_ACK:
+				// TODO what to do here?
+				break;
+			default:
+				throw new IllegalArgumentException("TinyOsSerialDecoder received unknown packet type: " + packetType);
+		}
 
 		final ChannelBuffer decoded = ChannelBuffers.buffer(encodedBufferLength - 3);
-
-		decoded.writeByte(firstByte);
-
-		int crcCalculated = 0;
 
 		int crcRead = (encoded.getByte(encoded.readerIndex() + encoded.readableBytes() - 2) & 0xff)
 				| (encoded.getByte(encoded.readerIndex() + encoded.readableBytes() - 1) & 0xff) << 8;
 
+		int crcCalculated = 0;
 		crcCalculated = TinyOsCrc.calcByte(crcCalculated, packetType);
-		crcCalculated = TinyOsCrc.calcByte(crcCalculated, firstByte);
-
-		int payloadBegin = encoded.readerIndex() + 2;
-		int payloadEnd = encoded.writerIndex() - 2;
+		crcCalculated = TinyOsCrc.calcByte(crcCalculated, sequenceNumber);
 
 		byte currentByte;
 		for (int i = payloadBegin; i < payloadEnd; i++) {
@@ -56,7 +73,7 @@ public class TinyOsSerialDecoder extends OneToOneDecoder {
 		}
 
 		if (crcCalculated != crcRead) {
-			log.trace("CRC mismatch, discarding packet {}", encoded);
+			log.warn("CRC mismatch, discarding packet {}", decoded);
 			return null;
 		}
 
