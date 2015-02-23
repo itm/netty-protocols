9fb4d113c56ee7982b138f3a221c424283f16613
Daniel Bimschas <daniel@bimschas.com>
2015-02-23 21:43:43 +0100

    Trying to find out why build fails on CI

--- src/test/java/de/uniluebeck/itm/nettyprotocols/tinyos/TinyOsSerialDecoderTest.java
+++ src/test/java/de/uniluebeck/itm/nettyprotocols/tinyos/TinyOsSerialDecoderTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -29,22 +30,26 @@ public class TinyOsSerialDecoderTest implements TinyOsSerialTestConstants {
 
     @Test
     public void testPacket1() throws Exception {
-        assertTrue(DECODED_PACKET_1.equals(decoderTinyOs.decode(null, null, ENCODED_PACKET_1)));
+        Object actual = decoderTinyOs.decode(null, null, ENCODED_PACKET_1);
+        assertEquals(DECODED_PACKET_1.toString() + " is not equal to " + actual, DECODED_PACKET_1, actual);
     }
 
     @Test
     public void testPacket2() throws Exception {
-        assertTrue(DECODED_PACKET_2.equals(decoderTinyOs.decode(null, null, ENCODED_PACKET_2)));
+        Object actual = decoderTinyOs.decode(null, null, ENCODED_PACKET_2);
+        assertEquals(DECODED_PACKET_2.toString() + " is not equal to " + actual, DECODED_PACKET_2, actual);
     }
 
     @Test
     public void testPacket3() throws Exception {
-        assertTrue(DECODED_PACKET_3.equals(decoderTinyOs.decode(null, null, ENCODED_PACKET_3)));
+        Object actual = decoderTinyOs.decode(null, null, ENCODED_PACKET_3);
+        assertEquals(DECODED_PACKET_3.toString() + " is not equal to " + actual, DECODED_PACKET_3, actual);
     }
 
     @Test
     public void testPacket4() throws Exception {
-        assertTrue(DECODED_PACKET_4.equals(decoderTinyOs.decode(null, null, ENCODED_PACKET_4)));
+        Object actual = decoderTinyOs.decode(null, null, ENCODED_PACKET_4);
+        assertEquals(DECODED_PACKET_4.toString() + " is not equal to " + actual, DECODED_PACKET_4, actual);
     }
 
     @Test
